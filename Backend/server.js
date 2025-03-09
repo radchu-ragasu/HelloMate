e5981b3e699d198b3274380eaea2cedec911a462
Pasindu <163386074+pasindupiumal03@users.noreply.github.com>
2025-03-10 00:52:15 +0530

    changed the port

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send('Node.js Backend is running!');
 });
