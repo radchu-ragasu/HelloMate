2416af63d47efbbe87d249947ca1405ef63061ab
lakae <lakeshan2224@gmail.com>
2025-03-15 10:28:19 +0530

    links page

--- Ai_Bot/components/ExternalLink.tsx
+++ Ai_Bot/components/ExternalLink.tsx
@@ -5,6 +5,7 @@ import { Platform } from 'react-native';
 
 type Props = Omit<ComponentProps<typeof Link>, 'href'> & { href: string };
 
+
 export function ExternalLink({ href, ...rest }: Props) {
   return (
     <Link
