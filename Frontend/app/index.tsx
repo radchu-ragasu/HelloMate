8f2d78a9011326b5df890e467430b66a581e67aa
Mohamed Aathik <mohamed.20232359@iit.ac.lk>
2025-03-18 14:28:15 +0530

    updated scrn

--- Frontend/app/index.tsx
+++ Frontend/app/index.tsx
@@ -1,27 +1,20 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import OrderDetailScreen from "./order details/OrderScreen";
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import  AppNavigator  from './navigation/Navigator'; // Changed to named import
 
-const Stack = createStackNavigator();
-//navigator 
-const StackNavigator = () => {
-  return (
-      <Stack.Navigator initialRouteName="OrderScreen">
-      <Stack.Screen 
-          name="OrderScreen" 
-          component={OrderDetailScreen} 
-          options={{ title: "Order Detail" }} 
-        />
-        </Stack.Navigator>
-  );
-};
+
 const App = () => {
   return (
-    <NavigationContainer>
-      {/* ✅ Using the StackNavigator function here */}
-      <StackNavigator />
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
-export default App;
+
+// Instead of importing from app.json
+const appName = 'HelloMate'; // Or whatever your app name is
+AppRegistry.registerComponent(appName, () => App);
