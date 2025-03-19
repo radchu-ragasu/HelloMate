50f4c3feebeb8d2d1eb109fd096ba9d7067602d8
Mohamed Aathik <mohamed.20232359@iit.ac.lk>
2025-03-19 10:54:32 +0530

    upto

---
+++ Frontend/app/navigation/AppNavigator.tsx
@@ -0,0 +1,21 @@
+// In your navigation file (e.g., AppNavigator.tsx)
+import { createStackNavigator } from '@react-navigation/stack';
+import OrderScreen6 from '../order details/OrderScreen6';
+import { RootStackParamList } from '../navigation/navigation';
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+function AppNavigator() {
+  return (
+    <Stack.Navigator>
+      
+      <Stack.Screen 
+        name="OrderScreen6" 
+        component={OrderScreen6} 
+        options={{ headerShown: false }} // Since we have our own header
+      />
+      {/* Other screens */}
+    </Stack.Navigator>
+  );
+}
+export default AppNavigator;
